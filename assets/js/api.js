54313e46bf190f7e03c9b889b857c2f749bf5479
Vibhakar <vibhakarsenth@gmail.com>
2025-09-29 21:53:32 +0530

    Configured Routes

--- assets/js/api.js
+++ assets/js/api.js
@@ -15,22 +15,22 @@ export const fetchData = function(URL, callback) {
 
 export const url = {
     currentWeather(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
     },
     forecast(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
     },
     airPollution(lat, lon) {
-        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
     },
     reverseGeo(lat, lon) {
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
     },
     /**
      * 
      * @param {string} query Search Query e.g: "London"
      */
     geo(query) {
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
     }
 }
